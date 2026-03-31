1fcce3460ea94a61e8578e731339589d75b340f0
Sebastian Buczyński <nnplaya@gmail.com>
2026-03-31 14:15:07 +0200

    Remove 'dunder' from task description because it can be misleading

--- classes/tasks/task_3.py
+++ classes/tasks/task_3.py
@@ -2,6 +2,6 @@
 # Point should consist of two fields - x and y coordinates.
 # Segment should consist of two points - a and b.
 #
-# Add __len__ dunder method to Segment that will calculate length of the Segment.
+# Add `__len__` method to Segment that will calculate length of the Segment.
 # The formula is math.sqrt((point_2.x - point_1.x)**2 + (point_2.y - point_1.y)**2)
 import math
